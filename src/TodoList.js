b79b92d749a94adc7ebf109f5c105505f750a5b1
Sujin <shtnwls1111@ewhain.net>
2023-03-24 22:45:54 +0900

    localStorage

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,53 @@
-import React, { useCallback, useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import AddForm from './AddForm';
 import TodoText from './TodoText';
 import './style.css';
 
 function TodoList() {
+  const [todoList, setTodoList] = useState([]);
+  const [todoId, setTodoId] = useState(0);
+  const [doneList, setDoneList] = useState([]);
+  const [doneId, setDoneId] = useState(0);
+  const isMount = useRef(true);
+
+  useEffect(() => {
+    if (!isMount.current) {
+      localStorage.setItem('todoList', JSON.stringify(todoList));
+      localStorage.setItem('todoId', todoId);
+      console.log('todo');
+
+      localStorage.setItem('doneList', JSON.stringify(doneList));
+      localStorage.setItem('doneId', doneId);
+      console.log('done');
+    }
+  }, [todoList, todoId, doneList, doneId]);
+
+  useEffect(() => {
+    const localTodoList = localStorage.getItem('todoList');
+    if (localTodoList) {
+      setTodoList(JSON.parse(localTodoList));
+    }
+    const localTodoId = localStorage.getItem('todoId');
+    if (localTodoId) {
+      setTodoId(parseInt(localTodoId));
+    }
+
+    const localDoneList = localStorage.getItem('doneList');
+    if (localDoneList) {
+      setDoneList(JSON.parse(localDoneList));
+    }
+    const localDoneId = localStorage.getItem('doneId');
+    if (localDoneId) {
+      setDoneId(parseInt(localDoneId));
+    }
+
+    isMount.current = false;
+  }, []);
+
   return (
     <div className="container">
       <h1 className="title">Things to do</h1>
-      <AddForm />
+      <AddForm addTodo={addTodo} />
       <hr />
       <div className="todo">
         <p className="subTitle">📒to do</p>
@@ -21,15 +61,7 @@ function TodoList() {
         <p className="subTitle">📂done</p>
         <p className="doneCount">items: {doneId}</p>
         <ul type="none" id="doneList">
-          <TodoText
-            key={doneInfo.id}
-            id={doneInfo.id}
-            todo={doneInfo.todo}
-            isChecked={doneInfo.isChecked}
-            deleteTodo={deleteDone}
-            move={moveToTodo}
-            moveList={moveTodoList}
-          />
+          <TodoText />
         </ul>
       </div>
     </div>
